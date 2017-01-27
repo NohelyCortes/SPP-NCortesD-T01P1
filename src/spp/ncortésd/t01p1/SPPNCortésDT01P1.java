5cd1cd9ad8f650010b6044335c3a609b569d416a
NohelyCortes <A01410768@itesm.mx>
2017-01-26 23:05:36 -0600

    Update SPPNCortésDT01P1.java

--- src/spp/ncortésd/t01p1/SPPNCortésDT01P1.java
+++ src/spp/ncortésd/t01p1/SPPNCortésDT01P1.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+*Nohely Monserrat Cortés del Ángel
+*A01410768
+*Ingeniería Mecatrónica
  */
 package spp.ncortésd.t01p1;
 import java.util.Scanner;
